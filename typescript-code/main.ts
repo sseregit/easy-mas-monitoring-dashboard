5c2b20ff88f4c9fc4aec52698e75909afb546315
장연식 <sseregit@knowwheresoft.com>
2025-04-18 11:53:57 +0900

    TypeScript에서 HTTP를 활용한 OpenTracing 다루기

--- typescript-code/main.ts
+++ typescript-code/main.ts
@@ -1,18 +1,36 @@
-import initTracer from "./src/jaeger/root"
-import {opentracing} from "jaeger-client";
+import { opentracing } from "jaeger-client";
+import initTracer from "./src/jaeger/root";
+import * as express from "express";
 
 console.log("test");
 
 initTracer("test_tracer");
 
-const tracer: opentracing.Tracer = opentracing.globalTracer();
+const app = express();
 
-const span = tracer.startSpan("hello_test");
-span.setTag("hello_hello", "test_hello");
+app.get("/api/resource", (req, res) => {
+    const tracer: opentracing.Tracer = opentracing.globalTracer();
 
-span.log({
-    event: "string",
-    value: "string test",
+    const headersCarieer = req.headers;
+    const spaneContext = tracer.extract(
+        opentracing.FORMAT_HTTP_HEADERS,
+        headersCarieer
+    )
+
+    const span = tracer.startSpan("hello_test", {childOf: spaneContext});
+    span.setTag("hello_hello", "test_hello");
+    span.setTag(opentracing.Tags.HTTP_METHOD, "GET");
+    span.setTag(opentracing.Tags.HTTP_URL, "/api/resource");
+
+    span.log({
+        event: "string",
+        value: "string test",
+    });
+
+    span.finish();
+    res.send("Success")
 });
 
-span.finish();
+app.listen(8080, () => {
+    console.log("server started at", 8080);
+});
